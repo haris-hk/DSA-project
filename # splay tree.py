fc4ca9ed6e0ced7fef8c5382d48a41963033ff2d
haris-hk <66428634+haris-hk@users.noreply.github.com>
2024-04-09 20:33:22 +0500

    splay tree added , not completely built yet

---
+++ # splay tree.py
@@ -0,0 +1,108 @@
+# splay tree
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class SplayTree:
+    def __init__(self):
+        self.root = None
+
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right:
+            y.right.parent = x
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left:
+            y.left.parent = x
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def splay(self, x):
+        while x.parent:
+            if not x.parent.parent:
+                if x == x.parent.left:
+                    self.right_rotate(x.parent)
+                else:
+                    self.left_rotate(x.parent)
+            elif x == x.parent.left and x.parent == x.parent.parent.left:
+                self.right_rotate(x.parent.parent)
+                self.right_rotate(x.parent)
+            elif x == x.parent.right and x.parent == x.parent.parent.right:
+                self.left_rotate(x.parent.parent)
+                self.left_rotate(x.parent)
+            elif x == x.parent.right and x.parent == x.parent.parent.left:
+                self.left_rotate(x.parent)
+                self.right_rotate(x.parent)
+            else:
+                self.right_rotate(x.parent)
+                self.left_rotate(x.parent)
+
+    def insert(self, key, value):
+        node = Node(key, value)
+        y = None
+        x = self.root
+        while x:
+            y = x
+            if node.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        node.parent = y
+        if y is None:
+            self.root = node
+        elif node.key < y.key:
+            y.left = node
+        else:
+            y.right = node
+        self.splay(node)
+
+    def search(self, key):
+        x = self.root
+        while x:
+            if key < x.key:
+                x = x.left
+            elif key > x.key:
+                x = x.right
+            else:
+                self.splay(x)
+                return x.value
+        return None
+
+# Usage
+monopoly_properties = [
+    ["GO", "Collect $200 salary as you pass"],
+    ["Park Place", "Cost: $350"],
+    ["Boardwalk", "Cost: $400"],
+    # Add more properties as needed
+]
+
+tree = SplayTree()
+for key, value in monopoly_properties:
+    tree.insert(key, value)
+
+print(tree.search("Park Place"))  # Output: Cost: $350
